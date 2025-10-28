df5f3d54e98c9f96d08933438eb2cf475123574e
thomas almeida <contato.thomasalmeidard@gmail.com>
2025-10-27 22:34:10 -0300

    [Back] Removing uploads

--- backend/src/routes/uploadRoutes.js
+++ backend/src/routes/uploadRoutes.js
@@ -5,6 +5,6 @@ const router = Router();
 
 // Rota para upload de imagens
 // POST /api/upload/image
-router.post('/image', upload.single('image'), uploadImage);
+//router.post('/image', upload.single('image'), uploadImage);
 
 export default router;
