00bcd77dba5c67394d3d92d08e2b7a70a6ff2cf3
thomas almeida <contato.thomasalmeidard@gmail.com>
2025-12-03 12:41:46 -0300

    [Back] Fix username

--- backend/src/db/models/User.js
+++ backend/src/db/models/User.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
+    username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     store: {
         type: mongoose.Schema.Types.ObjectId,
