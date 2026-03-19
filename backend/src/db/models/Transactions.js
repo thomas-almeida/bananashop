92a6b1d89603dea3f1f58d5c5e23ef4eab8a0591
thomas-almeida <contato.thomasalmeidard@gmail.com>
2026-03-19 15:05:21 -0300

    [BACK] New Field Shipping Option

--- backend/src/db/models/Transactions.js
+++ backend/src/db/models/Transactions.js
@@ -16,6 +16,7 @@ const TransactionSchema = new mongoose.Schema({
         address: { type: String, required: true },
         city: { type: String, required: true },
         state: { type: String, required: true },
+        shipOption: { type: String, required: true }
     },
     status: { type: String, default: "PENDING" }
 });
